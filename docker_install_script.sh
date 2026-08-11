4cb3bcdb181b8f534110424cb0fa650c1ee5efc5
shareef-shaik <148448681+shareef-sk786@users.noreply.github.com>
2026-08-11 16:16:47 +0530

    Create docker_install_script.sh

---
+++ docker_install_script.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+
+sudo apt update -y
+
+apt install -y ca-certificates curl
+
+# Docker repository
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo cat > /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: noble
+Components: stable
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update -y
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+# Allow ubuntu user to use Docker
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
+sudo systemctl restart dokcer
+sudo chmod 777 /var/run/docker.sock
